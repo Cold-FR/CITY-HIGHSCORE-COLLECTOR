4c1b957b0e2afbebfb1319491787a5b34cb523ab
davi0063 <clement.david1@etudiant.univ-reims.fr>
2026-06-04 09:21:47 +0200

    refactor: add content and popup scripts with TypeScript implementation

---
+++ src/scripts/popup.ts
@@ -0,0 +1,129 @@
+/**
+ * @file This file contains the popup script.
+ * @author Cold-FR
+ * @last-modified 2025-02-08
+ * @version 1.1.2
+ * @github https://github.com/Cold-FR/CITY-QUEST-COLLECTOR
+ */
+
+/**
+ * @type {HTMLDivElement} - The players list.
+ * @default document.getElementById('players-list')
+ * @readonly
+ */
+const playersList: HTMLDivElement = document.getElementById('players-list') as HTMLDivElement;
+
+/**
+ * @type {HTMLFormElement} - The select ranking form.
+ * @default document.getElementById('select-ranking')
+ * @readonly
+ */
+const form: HTMLFormElement = document.getElementById('select-ranking') as HTMLFormElement;
+
+/**
+ * The form submit event.
+ * @function
+ * @param {Event} e - The event.
+ * @returns {void}
+ * @listens submit
+ */
+form.addEventListener('submit', (e: SubmitEvent) => {
+  e.preventDefault();
+
+  const submitter = e.submitter as HTMLInputElement;
+  if (submitter.classList.contains('clicked')) return;
+
+  (document.getElementById('select-ranking-btn') as HTMLButtonElement).classList.add('clicked');
+
+  return sendListenMessage();
+});
+
+/**
+ * The document ready event.
+ * @function
+ * @returns {void}
+ * @listens DOMContentLoaded
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  displayPlayersList();
+
+  chrome.storage.local.get('darkMode', (data) => {
+    if (data.darkMode) document.documentElement.classList.add('black');
+  });
+});
+
+/**
+ * The color mode button click event.
+ * @function
+ * @returns {void}
+ * @listens click
+ */
+(document.getElementById('colorMode') as HTMLButtonElement).addEventListener('click', () => {
+  toggleColorMode();
+});
+
+/**
+ * Toggles the color mode.
+ * @function
+ * @returns {void}
+ */
+function toggleColorMode(): void {
+  if (document.documentElement.classList.contains('black')) {
+    chrome.storage.local
+      .set({ darkMode: false })
+      .then(() => document.documentElement.classList.remove('black'));
+  } else {
+    chrome.storage.local
+      .set({ darkMode: true })
+      .then(() => document.documentElement.classList.add('black'));
+  }
+}
+
+/**
+ * Displays the ignored values list.
+ * @function
+ * @returns {void}
+ */
+function displayPlayersList(): void {
+  playersList.innerHTML = '';
+
+  chrome.storage.local.get('players', (data: { players: string[] }) => {
+    playersList.innerHTML = '';
+
+    if (data.players && data.players.length > 0) {
+      const textArea = document.createElement('textarea');
+      textArea.readOnly = true;
+      textArea.value = data.players.join(',');
+      playersList.appendChild(textArea);
+
+      const copyButton = document.createElement('button');
+      copyButton.innerText = 'Copier';
+      copyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(textArea.value).then(() => {
+          copyButton.innerText = 'Copié !';
+          setTimeout(() => (copyButton.innerText = 'Copier'), 1000);
+        });
+      });
+      playersList.appendChild(copyButton);
+    } else {
+      const p = document.createElement('p');
+      p.classList.add('empty');
+      p.innerText = `Aucun joueur dans ce classement.`;
+      playersList.appendChild(p);
+    }
+  });
+}
+
+/**
+ * Sends a message to the content script to listen to click event on rankings.
+ * @function
+ * @returns {void}
+ */
+function sendListenMessage(): void {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+    if (!(activeTab.url as string).match('habbocity.fr')) return;
+
+    chrome.tabs.sendMessage(activeTab.id as number, 'listenClick');
+  });
+}
