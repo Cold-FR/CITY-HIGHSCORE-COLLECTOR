b081e38d351a90fe7dfbf6b66467b3ea2745e951
Clément <51854565+Cold-FR@users.noreply.github.com>
2026-06-12 15:41:08 +0200

    chore: Update src/scripts/popup.ts last modified date

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,7 +1,7 @@
 /**
  * @file This file contains the popup script.
  * @author Cold-FR
- * @last-modified 2025-02-08
+ * @last-modified 2025-06-04
  * @version 1.2.0
  * @github https://github.com/Cold-FR/CITY-QUEST-COLLECTOR
  */
